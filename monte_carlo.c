664b927e1ec4c234a7e1495e8b63ddc070d9ba0f
GopisettiPoojitha <84663421+GopisettiPoojitha@users.noreply.github.com>
2021-06-02 22:50:05 +0530

    solution monte_carlo

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,21 +3,35 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
-
 float frandom() {
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
   return ret;
 }
+float mc_pi(int count)
+{
+	float circount= 0;
+	for(int i=0;i < count;i++)
+	{
+               float x = frandom();
+		float y = frandom();
+		float z = sqrt((x*x+y*y));
+		if( z < 1)
+			circount++;
+		}
+	float pi = (4*((float)circount/count));
+	return pi;
+	}
 
 int main(void) {
   float pi0;
   float pi1;
   
-  pi0 = mc_pi(25000);
-  pi1 = mc_pi(25000);
+  pi0 = mc_pi(200000);
+  pi1 = mc_pi(200000);
+  float error = pi1 - pi0;
   printf("%f %f\n", pi0, pi1);
+  // printf("%f\n", error);
   
   if (pi0 == pi1) {
       printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
@@ -38,6 +52,8 @@ int main(void) {
     }
   }
 }
+
+
 
 
 
